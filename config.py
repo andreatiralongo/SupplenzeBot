157702bed3583a8801abf74963d00eaef4c301d3
andreatiralongo <56975988+andreatiralongo@users.noreply.github.com>
2025-09-14 20:17:07 +0200

    Add Github Actions Workflow

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
-URL = ""
-TEXT_SEARCH = ""
-API_KEY = ""
+import os
+
+URL = os.getenv("URL")
+TEXT_SEARCH = os.getenv("TEXT_SEARCH")
+API_KEY = os.getenv("TELEGRAM_TOKEN")
 MINUTES_TIMEOUT = 15
 
-CHAT_IDS = []
+CHAT_IDS = os.getenv("TELEGRAM_CHAT_IDS").split(",")
